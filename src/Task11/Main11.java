70dfb81eb4241722c7a13a3ee039101783dea5c6
LizzieGri <Liza-gri2001@yandex.ru>
2020-10-10 18:16:32 +0300

    Task11

---
+++ src/Task11/Main11.java
@@ -0,0 +1,29 @@
+package Task11;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main11 extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello World!");
+
+        StackPane root = new StackPane();
+        root.getChildren().add(label);
+
+        Scene scene = new Scene(root, 400, 300);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
