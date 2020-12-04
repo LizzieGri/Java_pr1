4cdd37aa99ea0540b6f57efc68fc93d508f08eb5
LizzieGri <Liza-gri2001@yandex.ru>
2020-12-04 13:41:33 +0300

    Task25-26

---
+++ src/Task25/Main25.java
@@ -0,0 +1,9 @@
+package Task25;
+
+import java.io.EOFException;
+
+public class Main25 {
+    public static void main(String[] args) throws EOFException {
+        Parser parser = new Parser();
+    }
+}
